b79b7912a38a177a2d34d9e813023b1be83b714b
samemaru <66pistols@gmail.com>
2020-05-02 23:37:01 +0900

    add header

--- src/App.js
+++ src/App.js
@@ -13,13 +13,17 @@ library.add(fab)//あらかじめ使用するアイコンを追加しておく
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a href="index.html">samemaru</a>
-        <a href="https://twitter.com/samemaru_saxo" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'twitter']} /></a>
+      <header className="header">
+        <h1 className="App_logo"><a href="index.html">samemaru</a></h1>
+        <nav className="global-nav">
+          <li className="nav_item" ><a href="#">WORK</a></li>
+          <li className="nav_item" ><a href="#">ABOUT</a></li>
+          <li className="nav_item" ><a href="https://twitter.com/samemaru_saxo" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'twitter']} /></a></li>
+        </nav>
       </header>
-      <body>
-        <img src={logo} className="App-logo" alt="logo" />  
-      </body>
+      <div className="wrapper">
+        <img src={logo} className="App_icon" alt="App_icon" />  
+      </div>
     </div>
   );
 }
