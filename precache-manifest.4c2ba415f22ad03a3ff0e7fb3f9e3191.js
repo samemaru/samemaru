4ecece7b559bbddd43de0b8aa79e721c8eea8deb
samemaru <66pistols@gmail.com>
2020-05-06 00:12:33 +0900

    Updates

---
+++ precache-manifest.4c2ba415f22ad03a3ff0e7fb3f9e3191.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a2a2936f512cb78baada5f8e1570b5",
+    "url": "/samemaru/index.html"
+  },
+  {
+    "revision": "c2b9de60e8e8158288e1",
+    "url": "/samemaru/static/css/main.11cc53f4.chunk.css"
+  },
+  {
+    "revision": "f2654d451b09cc9b84c9",
+    "url": "/samemaru/static/js/2.6c75f0f8.chunk.js"
+  },
+  {
+    "revision": "c2b9de60e8e8158288e1",
+    "url": "/samemaru/static/js/main.6b4bfd07.chunk.js"
+  },
+  {
+    "revision": "37c2b98a30a304dfbf3b",
+    "url": "/samemaru/static/js/runtime-main.3953db29.js"
+  },
+  {
+    "revision": "4adf22f20a4b9238fe2b13c74117f922",
+    "url": "/samemaru/static/media/logo.4adf22f2.png"
+  }
+]);
